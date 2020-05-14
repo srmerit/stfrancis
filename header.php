afeb74840b8f06c1b78940ce2c6b054066a7d827
srmerit <56222598+srmerit@users.noreply.github.com>
2020-05-14 17:41:17 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="APPTON HTML5 Template is a simple Smooth Personal App Landing Template" />
+    <meta name="keywords" content="App, Landing, Business, Onepage, Html, Business" />
+
+    <title>St. Francis Parish Basirika C.O.U</title>
+
+    <link rel="shortcut icon" type="image/ico" href="assest/img/favicon.png" />
+
+    <link href="assest/css/plugins.css" rel="stylesheet">
+    <link href="assest/css/theme.css" rel="stylesheet">
+    <link href="assest/css/icons.css" rel="stylesheet">
+
+    <link href="style.css" rel="stylesheet">
+    <link href="assest/css/responsive.css" rel="stylesheet">
+
+    <script src="assest/js/vendor/modernizr-2.8.3.min.js"></script>
+
+</head>
